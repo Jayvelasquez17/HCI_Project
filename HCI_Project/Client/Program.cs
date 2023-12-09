2a67a4e4cfb78113087d5cc588834207495e42b5
Jesus Velasquez <jesusvelasquez@mail.weber.edu>
2023-12-08 18:21:34 -0700

    added all images, modal, text size toggle

--- HCI_Project/Client/Program.cs
+++ HCI_Project/Client/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HCI_Project.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddRadzenComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
